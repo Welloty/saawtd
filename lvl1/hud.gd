2f3371491ae17428bd2b3f4a9358fae4ada9d04d
Melolr0Focus <welloty403@gmail.com>
2026-07-22 15:44:36 +0300

    gui

---
+++ lvl1/hud.gd
@@ -0,0 +1,6 @@
+extends CanvasLayer
+
+@onready var money_value_label: Label = $Control/MarginContainer/HBoxContainer/MoneyPanel/HBoxContainer/VBoxContainer/MoneyValueLabel
+@onready var wave_value_label: Label = $Control/MarginContainer/HBoxContainer/WavePanel/HBoxContainer/VBoxContainer/WaveValueLabel
+
+# гуишка без логики
